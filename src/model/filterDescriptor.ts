3d0ab33cf2de82c4df175985651e34fee60feded
wz2cool <wz2cool@live.cn>
2017-10-11 18:48:38 +0800

    add

---
+++ src/model/filterDescriptor.ts
@@ -0,0 +1,29 @@
+import { CommonsHelper } from "./../helper/commonsHelper";
+import { FilterCondition } from "./filterCondition";
+import { FilterOperator } from "./filterOperator";
+
+export class FilterDescriptor<T> {
+    public condition: FilterCondition = FilterCondition.AND;
+    public operator: FilterOperator = FilterOperator.EQUAL;
+    public propertyPath: string;
+    public value: boolean | number | string;
+
+    public constructor(
+        condition?: FilterCondition,
+        getPropFun?: (t: T) => any,
+        operator?: FilterOperator,
+        value?: boolean | number | string) {
+        if (condition) {
+            this.condition = condition;
+        }
+        if (getPropFun) {
+            this.propertyPath = CommonsHelper.getPropertyName<T>(getPropFun);
+        }
+        if (operator) {
+            this.operator = operator;
+        }
+        if (value) {
+            this.value = value;
+        }
+    }
+}
