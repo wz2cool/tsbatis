5e6b2389eb46f6ca931e0847ecedfff2a487a244
wz2cool <wz2cool@live.cn>
2017-10-13 18:01:01 +0800

    add

--- src/mapper/baseMapper.ts
+++ src/mapper/baseMapper.ts
@@ -1,47 +1,48 @@
 import * as lodash from "lodash";
-import { EntityCache } from "../cache";
+
 import { CommonHelper, EntityHelper } from "../helper";
 import { ISqlQuery } from "../model";
 import { SqlProvider } from "../provider";
 
 export abstract class BaseMapper<T> {
-    protected readonly cache: EntityCache = EntityCache.getInstance();
     protected readonly sqlQuery: ISqlQuery;
     constructor(sqlQuery: ISqlQuery) {
         this.sqlQuery = sqlQuery;
     }
 
     public insert(o: T, cb: (err: any, result?: any) => void): void {
-        const entityName = EntityHelper.getEntityName(o);
-        if (CommonHelper.isNullOrUndefined(entityName)) {
-            if (cb) { cb(new Error("cannot find entity, please set @column to entity!")); }
-            return;
+        try {
+            const sqlParam = SqlProvider.getInsert<T>(o, false);
+            this.sqlQuery.query(sqlParam.sqlExpression, sqlParam.params, cb);
+        } catch (e) {
+            if (cb) { cb(e); }
         }
-
-        const columnInfos = this.cache.getColumnInfos(entityName);
-        if (CommonHelper.isNullOrUndefined(columnInfos) || columnInfos.length === 0) {
-            if (cb) { cb(new Error("cannot find entity, please set @column to entity!")); }
-            return;
-        }
-
-        const sqlParam = SqlProvider.getInsert<T>(o, columnInfos, false);
-        this.sqlQuery.query(sqlParam.sqlExpression, sqlParam.params, cb);
     }
 
     public insertSelective(o: T, cb: (err: any, result?: any) => void): void {
-        const entityName = EntityHelper.getEntityName(o);
-        if (CommonHelper.isNullOrUndefined(entityName)) {
-            if (cb) { cb(new Error("cannot find entity, please set @column to entity!")); }
-            return;
+        try {
+            const sqlParam = SqlProvider.getInsert<T>(o, true);
+            this.sqlQuery.query(sqlParam.sqlExpression, sqlParam.params, cb);
+        } catch (e) {
+            if (cb) { cb(e); }
         }
+    }
 
-        const columnInfos = this.cache.getColumnInfos(entityName);
-        if (CommonHelper.isNullOrUndefined(columnInfos) || columnInfos.length === 0) {
-            if (cb) { cb(new Error("cannot find entity, please set @column to entity!")); }
-            return;
+    public updateByKey(o: T, cb: (err: any, result?: any) => void): void {
+        try {
+            const sqlParam = SqlProvider.getUpdateByKey<T>(o, false);
+            this.sqlQuery.query(sqlParam.sqlExpression, sqlParam.params, cb);
+        } catch (e) {
+            if (cb) { cb(e); }
         }
+    }
 
-        const sqlParam = SqlProvider.getInsert<T>(o, columnInfos, true);
-        this.sqlQuery.query(sqlParam.sqlExpression, sqlParam.params, cb);
+    public updateSelectiveByKey(o: T, cb: (err: any, result?: any) => void): void {
+        try {
+            const sqlParam = SqlProvider.getUpdateByKey<T>(o, true);
+            this.sqlQuery.query(sqlParam.sqlExpression, sqlParam.params, cb);
+        } catch (e) {
+            cb(e);
+        }
     }
 }
--- src/provider/sqlProvider.ts
+++ src/provider/sqlProvider.ts
@@ -1,35 +1,103 @@
-import { CommonHelper } from "../helper";
+import * as lodash from "lodash";
+import { EntityCache } from "../cache";
+import { CommonHelper, EntityHelper } from "../helper";
 import { ColumnInfo, SqlParam } from "../model";
 
 export class SqlProvider {
-    public static getInsert<T>(o: T, columnInfos: ColumnInfo[], selective: boolean): SqlParam {
+    public static getInsert<T>(o: T, selective: boolean): SqlParam {
+        const entityName = EntityHelper.getEntityName(o);
+        if (CommonHelper.isNullOrUndefined(entityName)) {
+            throw new Error("cannot find entity, please set @column to entity!");
+        }
+
+        const columnInfos = EntityCache.getInstance().getColumnInfos(entityName);
+        if (CommonHelper.isNullOrUndefined(columnInfos) || columnInfos.length === 0) {
+            throw new Error("cannot find entity, please set @column to entity!");
+        }
+
         const columnNames: string[] = [];
-        const placeHolders: string[] = [];
+        const placeholders: string[] = [];
         const params: any[] = [];
         columnInfos.forEach((colInfo) => {
             if (!colInfo.insertable) {
                 return;
             }
 
-            const propValue = o[colInfo.property];
+            let propValue = o[colInfo.property];
             if (selective && CommonHelper.isNullOrUndefined(propValue)) {
                 return;
             }
-
-            params.push(CommonHelper.isNullOrUndefined(propValue) ? null : propValue);
+            propValue = CommonHelper.isNullOrUndefined(propValue) ? null : propValue;
+            params.push(propValue);
             columnNames.push(colInfo.columnName);
-            placeHolders.push("?");
+            placeholders.push("?");
         });
 
         const tableName = columnInfos[0].table;
         const columnNamesStr = columnNames.join(",");
-        const placeholderStr = placeHolders.join(",");
+        const placeholderStr = placeholders.join(",");
         const sqlExpression = `INSERT INTO ${tableName} (${columnNamesStr}) VALUES (${placeholderStr})`;
 
         const sqlParam = new SqlParam();
         sqlParam.sqlExpression = sqlExpression;
         sqlParam.params = params;
         return sqlParam;
+    }
+
+    public static getUpdateByKey<T>(o: T, selective: boolean): SqlParam {
+        const entityName = EntityHelper.getEntityName(o);
+        if (CommonHelper.isNullOrUndefined(entityName)) {
+            throw new Error("cannot find entity, please set @column to entity!");
+        }
+
+        const columnInfos = EntityCache.getInstance().getColumnInfos(entityName);
+        if (CommonHelper.isNullOrUndefined(columnInfos) || columnInfos.length === 0) {
+            throw new Error("cannot find entity, please set @column to entity!");
+        }
+
+        const keyColumn = lodash.find(columnInfos, (s) => s.isKey);
+        if (CommonHelper.isNullOrUndefined(keyColumn)) {
+            throw new Error("cannot find key, please set iskey property in @column.");
+        }
+
+        const tableName = columnInfos[0].table;
+        const sets: string[] = [];
+        const params: any[] = [];
+        columnInfos.forEach((colInfo) => {
+            if (colInfo.property === keyColumn.property) {
+                // don't update key column.
+                return;
+            }
+
+            let propValue = o[colInfo.property];
+            if (selective && propValue) {
+                return;
+            }
+
+            propValue = CommonHelper.isNullOrUndefined(propValue) ? null : propValue;
+            sets.push(colInfo.columnName + " = ?");
+            params.push(propValue);
+        });
+        const setStr = sets.join(", ");
+        const whereStr = keyColumn.columnName + " = ?";
+        params.push(o[keyColumn.property]);
+        const sqlExpression = `UPDATE ${tableName} SET ${setStr} WHERE ${whereStr}`;
+
+        const sqlParam = new SqlParam();
+        sqlParam.sqlExpression = sqlExpression;
+        sqlParam.params = params;
+        return sqlParam;
+    }
+
+    public static getSelectByKey<T>(o: T, columnInfos: ColumnInfo[]): SqlParam {
+        const keyColumn = lodash.find(columnInfos, (s) => s.isKey);
+        if (CommonHelper.isNullOrUndefined(keyColumn)) {
+            throw new Error("cannot find key, please set iskey property in @column.");
+        }
+    }
+
+    public static getColumnsAsUnderscoreProps(columnInfos: ColumnInfo[]): string {
+        return lodash.map(columnInfos, (s) => s.columnName + " AS " + s.underscoreProperty).join(", ");
     }
 
     private constructor() {
