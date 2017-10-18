15ed048a67ccade33675116f0a176ed84f04d3ee
wz2cool <wz2cool@live.cn>
2017-10-18 14:15:37 +0800

    add

--- src/model/customFilterDescriptor.ts
+++ src/model/customFilterDescriptor.ts
@@ -2,5 +2,11 @@ import { FilterDescriptorBase } from "./filterDescriptorBase";
 
 export class CustomFilterDescriptor extends FilterDescriptorBase {
     public expression: string;
-    public params: any[] = [];
+    public params: any[];
+
+    constructor() {
+        super();
+        this.expression = "";
+        this.params = [];
+    }
 }
--- src/model/customSortDescriptor.ts
+++ src/model/customSortDescriptor.ts
@@ -2,5 +2,11 @@ import { SortDescriptorBase } from "./sortDescritporBase";
 
 export class CustomSortDescriptor extends SortDescriptorBase {
     public expression: string;
-    public params: any[] = [];
+    public params: any[];
+
+    constructor() {
+        super();
+        this.expression = "";
+        this.params = [];
+    }
 }
