3e8a2451e8387b41dbc92ab611c53438af8084ce
wz2cool <wz2cool@live.cn>
2017-10-18 10:45:26 +0800

    add

--- test/helper/FilterHelper.ts
+++ test/helper/FilterHelper.ts
@@ -44,4 +44,69 @@ describe(".FilterHelper", () => {
             expect(1).to.be.eq(result);
         });
     });
+
+    describe("#getFilterValues", () => {
+        it("shoud return array if operator is 'IN' and value is array", () => {
+            const filterValues = [1, 2];
+            const result = (FilterHelper as any).getFilterValues(FilterOperator.IN, filterValues);
+            expect(filterValues).to.be.eq(result);
+        });
+
+        it("shoud return error if operator is 'IN' and value is 1", () => {
+            function test() {
+                (FilterHelper as any).getFilterValues(FilterOperator.IN, 1);
+            }
+            expect(test).to.throw(TypeError);
+        });
+
+        it("shoud return array if operator is 'NOT_IN' and value is array", () => {
+            const filterValues = [1, 2];
+            const result = (FilterHelper as any).getFilterValues(FilterOperator.NOT_IN, filterValues);
+            expect(filterValues).to.be.eq(result);
+        });
+
+        it("shoud return error if operator is 'NOT_IN' and value is 1", () => {
+            function test() {
+                (FilterHelper as any).getFilterValues(FilterOperator.NOT_IN, 1);
+            }
+            expect(test).to.throw(TypeError);
+        });
+
+        it("shoud return array if operator is 'BETWEEN' and value is array", () => {
+            const filterValues = [1, 2];
+            const result = (FilterHelper as any).getFilterValues(FilterOperator.BETWEEN, filterValues);
+            expect(filterValues).to.be.eq(result);
+        });
+
+        it("shoud return error if operator is 'BETWEEN' and value is 1", () => {
+            function test() {
+                (FilterHelper as any).getFilterValues(FilterOperator.BETWEEN, 1);
+            }
+            expect(test).to.throw(TypeError);
+        });
+
+        it("shoud return error if operator is 'BETWEEN' and value is [1,2,3]", () => {
+            function test() {
+                (FilterHelper as any).getFilterValues(FilterOperator.BETWEEN, [1, 2, 3]);
+            }
+            expect(test).to.throw(TypeError);
+        });
+
+        it("should return error if operator not 'IN', 'NOT_IN', 'BETWEEN', value is [1,2,3]", () => {
+            function test() {
+                (FilterHelper as any).getFilterValues(FilterOperator.EQUAL, [1, 2, 3]);
+            }
+            expect(test).to.throw(TypeError);
+        });
+
+        it("should return null if operator is 'EQUAL' and value is null", () => {
+            const result = (FilterHelper as any).getFilterValues(FilterOperator.EQUAL, null);
+            expect(null).to.be.eq(result[0]);
+        });
+
+        it("should return [1] if operator is 'EQUAL' and value is 1 ", () => {
+            const result = (FilterHelper as any).getFilterValues(FilterOperator.EQUAL, 1);
+            expect(1).to.be.eq(result[0]);
+        });
+    });
 });
