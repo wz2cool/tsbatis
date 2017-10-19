680bc3b59a716244d944e805e5082e5ab86ac947
wz2cool <wz2cool@live.cn>
2017-10-19 22:56:33 +0800

    add

---
+++ test/decorator/columnTest.ts
@@ -0,0 +1,47 @@
+import { expect } from "chai";
+import { EntityCache } from "../../src/cache";
+import { column } from "../../src/decorator";
+import { CommonHelper } from "../../src/helper";
+
+describe(".column", () => {
+    describe("#tableColumn", () => {
+        class Student {
+            @column("id", true, false)
+            public id: number;
+            @column("name")
+            public name: string;
+        }
+
+        const cache = EntityCache.getInstance();
+        it("id is key", () => {
+            const result = cache.getColumnInfo("Student", "id");
+            expect("id").to.be.eq(result.columnName);
+            expect(true).to.be.eq(result.isKey);
+            expect(false).to.be.eq(result.insertable);
+            expect(true).to.be.eq(CommonHelper.isBlank(result.table));
+        });
+    });
+
+    describe("#viewColumn", () => {
+        class ProductView {
+            @column("product_id", "product")
+            public id: number;
+            @column("product_name", "product")
+            public productName: string;
+            @column("category_name", "category")
+            public categoryName: string;
+        }
+        const cache = EntityCache.getInstance();
+        it("id column has table", () => {
+            const result = cache.getColumnInfo("ProductView", "id");
+            expect("product_id").to.be.eq(result.columnName);
+            expect(false).to.be.eq(result.isKey);
+            expect(true).to.be.eq(result.insertable);
+            expect("product").to.be.eq(result.table);
+        });
+
+        it("column throw exception if not annotion", () => {
+            expect(column("name")).to.be.throw(Error);
+        });
+    });
+});
