768f3fc6c5a08983e31ad1dc48e85ad44f0c497e
wz2cool <wz2cool@live.cn>
2017-10-24 13:54:46 +0800

    add

--- test/mysqldb/mysqlConnectionTest.ts
+++ test/mysqldb/mysqlConnectionTest.ts
@@ -1,5 +1,5 @@
 import * as mysql from "mysql";
-import { CommonHelper, MysqlConnection } from "../../src";
+import { CommonHelper, MysqlConnection, SqlTemplate } from "../../src";
 import { Customer } from "./entity/customer";
 
 describe("mysql connection test", () => {
@@ -51,7 +51,7 @@ describe("mysql connection test", () => {
                     }
                 }
             });
-        });
+        }).timeout(50000);
 
         it("selectCount should work", (done) => {
             mysqlConnection.selectCount("select count(0) from customers", [], (err, result) => {
@@ -65,6 +65,32 @@ describe("mysql connection test", () => {
                     }
                 }
             });
-        })
+        }).timeout(50000);
+
+        it("runTransaction should work", (done) => {
+            const creatTime = new Date();
+            const updateTime = new Date();
+            const insertStudent1Template = new SqlTemplate();
+            insertStudent1Template.sqlExpression =
+                `INSERT INTO student(name, age, create_time, update_time) VALUES (?,?,?,?)`;
+            insertStudent1Template.params = ["frank" + creatTime, 30, creatTime, updateTime];
+
+            const insertStudent2Template = new SqlTemplate();
+            insertStudent2Template.sqlExpression =
+                `INSERT INTO student(name, age, create_time, update_time) VALUES (?,?,?,?)`;
+            insertStudent2Template.params = ["marry" + creatTime, 30, creatTime, updateTime];
+
+            mysqlConnection.runTransaction([insertStudent1Template, insertStudent2Template], (err, result) => {
+                if (err) {
+                    done(err);
+                } else {
+                    if (result.length > 0) {
+                        done();
+                    } else {
+                        done("should have entities");
+                    }
+                }
+            })
+        }).timeout(50000);
     });
 });
