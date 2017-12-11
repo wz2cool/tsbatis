65f71ffe0a37ed7323b87ea52fc19c1a9e69b82a
wz2cool <wz2cool@live.cn>
2017-12-11 12:01:33 +0800

    add

--- test/connection/sqliteConnectionTest.ts
+++ test/connection/sqliteConnectionTest.ts
@@ -9,7 +9,7 @@ import { SqliteConnectionTestHelper } from "./sqliteConnectionTestHelper";
 describe(".mysqlConnection", () => {
     describe("#getDataBaseType", () => {
         it("should return mysql", () => {
-            const mysqlConnection = new MysqlConnection(null);
+            const mysqlConnection = new SqliteConnection(null);
             const result = mysqlConnection.getDataBaseType();
             expect(DatabaseType.MYSQL).to.be.eq(result);
         });
@@ -17,7 +17,7 @@ describe(".mysqlConnection", () => {
 
     describe("#getRowBoundsExpression", () => {
         it("should return `limit 20, 10` if rowbounds.offset is 20, rowbounds.limit 10", () => {
-            const mysqlConnection = new MysqlConnection(null);
+            const mysqlConnection = new SqliteConnection(null);
             const rowbounds = new RowBounds(20, 10);
             const result = mysqlConnection.getRowBoundsExpression(rowbounds);
             expect("limit 20, 10").to.be.eq(result);
