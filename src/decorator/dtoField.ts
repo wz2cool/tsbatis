2763991f37fc953c9663f3f53ebee26927a2c3d5
wz2cool <wz2cool@live.cn>
2018-01-08 11:07:38 +0800

    add

--- src/decorator/dtoField.ts
+++ src/decorator/dtoField.ts
@@ -4,8 +4,7 @@ import { DtoCache } from "../cache/dtoCache";
 import { CommonHelper } from "../helper/commonHelper";
 import { DtoFieldInfo } from "../model/dtoFieldInfo";
 
-export function dtoField(name: string);
-export function dtoField(a1?) {
+export function dtoField(name?: string) {
     const cache = DtoCache.getInstance();
     return (target: any, propertyKey: string) => {
         if (CommonHelper.isNullOrUndefined(target)
@@ -14,7 +13,8 @@ export function dtoField(a1?) {
             throw new Error("cannot find entity from target.constructor.name");
         }
 
-        const fieldName = typeof a1 === "undefined" ? propertyKey : a1;
+        const fieldName = typeof name === "undefined" || CommonHelper.isBlank(name)
+            ? propertyKey : name;
         const dtoObjectName = target.constructor.name;
         const propertyType = Reflect.getMetadata("design:type", target, propertyKey);
         const property = propertyKey;
