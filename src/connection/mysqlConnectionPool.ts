af0da40c8d546bfe2fb360a4e97c9be96d72fd6e
wz2cool <wz2cool@live.cn>
2018-08-22 13:51:16 +0800

    import with eval

--- src/connection/mysqlConnectionPool.ts
+++ src/connection/mysqlConnectionPool.ts
@@ -5,36 +5,36 @@ import { IConnectionPool } from "./iConnectionPool";
 import { MysqlConnection } from "./mysqlConnection";
 
 export class MysqlConnectionPool implements IConnectionPool {
-    private readonly pool: any;
-    private readonly enableLog: boolean;
-    constructor(config: MysqlConnectionConfig, enableLog: boolean) {
-        this.enableLog = enableLog;
-        const mysql = this.getDriver();
+  private readonly pool: any;
+  private readonly enableLog: boolean;
+  constructor(config: MysqlConnectionConfig, enableLog: boolean) {
+    this.enableLog = enableLog;
+    const mysql = this.getDriver();
 
-        this.pool = mysql.createPool({
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            user: config.user,
-            password: config.password,
-        });
-    }
+    this.pool = mysql.createPool({
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+    });
+  }
 
-    public getConnection(): Promise<IConnection> {
-        return new Promise<IConnection>((resolve, reject) => {
-            this.pool.getConnection((err, sqlConn) => {
-                if (CommonHelper.isNullOrUndefined(err)) {
-                    const result = new MysqlConnection(sqlConn, this.enableLog);
-                    resolve(result);
-                } else {
-                    reject(err);
-                }
-            });
-        });
-    }
+  public getConnection(): Promise<IConnection> {
+    return new Promise<IConnection>((resolve, reject) => {
+      this.pool.getConnection((err, sqlConn) => {
+        if (CommonHelper.isNullOrUndefined(err)) {
+          const result = new MysqlConnection(sqlConn, this.enableLog);
+          resolve(result);
+        } else {
+          reject(err);
+        }
+      });
+    });
+  }
 
-    private getDriver(): any {
-        // tslint:disable-next-line:no-implicit-dependencies
-        return require("mysql");
-    }
+  private getDriver(): any {
+    // tslint:disable-next-line:no-implicit-dependencies
+    return eval(`require("mysql")`);
+  }
 }
