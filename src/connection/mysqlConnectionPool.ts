9c2ff06696547a48fb956c3f903e03a44660b66f
wz2cool <wz2cool@live.cn>
2018-08-22 10:48:42 +0800

    revert

--- src/connection/mysqlConnectionPool.ts
+++ src/connection/mysqlConnectionPool.ts
@@ -5,36 +5,36 @@ import { IConnectionPool } from "./iConnectionPool";
 import { MysqlConnection } from "./mysqlConnection";
 
 export class MysqlConnectionPool implements IConnectionPool {
-  private pool: any;
-  private enableLog: boolean;
-  constructor(config: MysqlConnectionConfig, enableLog: boolean) {
-    this.enableLog = enableLog;
-    this.getDriver().then(mysql => {
-      this.pool = mysql.createPool({
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        user: config.user,
-        password: config.password,
-      });
-    });
-  }
+    private readonly pool: any;
+    private readonly enableLog: boolean;
+    constructor(config: MysqlConnectionConfig, enableLog: boolean) {
+        this.enableLog = enableLog;
+        const mysql = this.getDriver();
 
-  public getConnection(): Promise<IConnection> {
-    return new Promise<IConnection>((resolve, reject) => {
-      this.pool.getConnection((err, sqlConn) => {
-        if (CommonHelper.isNullOrUndefined(err)) {
-          const result = new MysqlConnection(sqlConn, this.enableLog);
-          resolve(result);
-        } else {
-          reject(err);
-        }
-      });
-    });
-  }
+        this.pool = mysql.createPool({
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            user: config.user,
+            password: config.password,
+        });
+    }
 
-  private async getDriver(): Promise<any> {
-    // tslint:disable-next-line:no-implicit-dependencies
-    return await import("mysql");
-  }
+    public getConnection(): Promise<IConnection> {
+        return new Promise<IConnection>((resolve, reject) => {
+            this.pool.getConnection((err, sqlConn) => {
+                if (CommonHelper.isNullOrUndefined(err)) {
+                    const result = new MysqlConnection(sqlConn, this.enableLog);
+                    resolve(result);
+                } else {
+                    reject(err);
+                }
+            });
+        });
+    }
+
+    private getDriver(): any {
+        // tslint:disable-next-line:no-implicit-dependencies
+        return require("mysql");
+    }
 }
