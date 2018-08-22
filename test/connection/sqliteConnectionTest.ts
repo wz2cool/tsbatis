6a13bb623858d61d6aff10a47d686dd82e9fa21e
wz2cool <wz2cool@live.cn>
2018-08-22 11:38:19 +0800

    add

--- test/connection/sqliteConnectionTest.ts
+++ test/connection/sqliteConnectionTest.ts
@@ -7,149 +7,159 @@ import { Customer } from "../db/entity/customer";
 import { SqliteConnectionTestHelper } from "./sqliteConnectionTestHelper";
 
 describe(".SqliteConnection", () => {
-    describe("#getDataBaseType", () => {
-        it("should return sqlite", () => {
-            const filepath = path.join(__dirname, "../../", "test", "northwind.db");
-            const sqliteConnection = new SqliteConnection(filepath);
-            const result = sqliteConnection.getDataBaseType();
-            expect(DatabaseType.SQLITE3).to.be.eq(result);
+  describe("#getDataBaseType", () => {
+    it("should return sqlite", async () => {
+      const filepath = path.join(__dirname, "../../", "test", "northwind.db");
+      const sqliteConnection = await new SqliteConnection(filepath).init();
+      const result = sqliteConnection.getDataBaseType();
+      expect(DatabaseType.SQLITE3).to.be.eq(result);
+    });
+  });
+
+  describe("#getRowBoundsExpression", () => {
+    it("should return `limit 20, 10` if rowbounds.offset is 20, rowbounds.limit 10", async () => {
+      const filepath = path.join(__dirname, "../../", "test", "northwind.db");
+      const sqliteConnection = await new SqliteConnection(filepath).init();
+      const rowbounds = new RowBounds(20, 10);
+      const result = sqliteConnection.getRowBoundsExpression(rowbounds);
+      expect("limit 20, 10").to.be.eq(result);
+    });
+  });
+
+  describe("#run", async () => {
+    const filepath = path.join(__dirname, "../../", "test", "northwind.db");
+    const mysqlConnection = await new SqliteConnection(filepath, true).init();
+    it("should run if valid sql", done => {
+      mysqlConnection
+        .run("SELECT 1", [])
+        .then(data => {
+          done();
+        })
+        .catch(err => {
+          done(err);
         });
     });
 
-    describe("#getRowBoundsExpression", () => {
-        it("should return `limit 20, 10` if rowbounds.offset is 20, rowbounds.limit 10", () => {
-            const filepath = path.join(__dirname, "../../", "test", "northwind.db");
-            const sqliteConnection = new SqliteConnection(filepath);
-            const rowbounds = new RowBounds(20, 10);
-            const result = sqliteConnection.getRowBoundsExpression(rowbounds);
-            expect("limit 20, 10").to.be.eq(result);
+    it("should has error if invalid sql", done => {
+      mysqlConnection
+        .run("invalid sql", [])
+        .then(data => {
+          done("should have error if sql is invalid");
+        })
+        .catch(err => {
+          done();
+        });
+    });
+  });
+
+  describe("#select", async () => {
+    const filepath = path.join(__dirname, "../../", "test", "northwind.db");
+    const mysqlConnection = await new SqliteConnection(filepath, true).init();
+    it("should run if valid sql", done => {
+      mysqlConnection
+        .select("SELECT 1", [])
+        .then(data => {
+          done();
+        })
+        .catch(err => {
+          done(err);
         });
     });
 
-    describe("#run", () => {
-        const filepath = path.join(__dirname, "../../", "test", "northwind.db");
-        const mysqlConnection = new SqliteConnection(filepath, true);
-        it("should run if valid sql", (done) => {
-            mysqlConnection.run("SELECT 1", [])
-                .then((data) => {
-                    done();
-                })
-                .catch((err) => {
-                    done(err);
-                });
+    it("should has error if invalid sql", done => {
+      mysqlConnection
+        .select("invalid sql", [])
+        .then(data => {
+          done("should have error if sql is invalid");
+        })
+        .catch(err => {
+          done();
         });
+    });
+  });
 
-        it("should has error if invalid sql", (done) => {
-            mysqlConnection.run("invalid sql", [])
-                .then((data) => {
-                    done("should have error if sql is invalid");
-                })
-                .catch((err) => {
-                    done();
-                });
+  describe("#selectCount", async () => {
+    const filepath = path.join(__dirname, "../../", "test", "northwind.db");
+    const mysqlConnection = await new SqliteConnection(filepath, true);
+    it("should run if valid sql", done => {
+      mysqlConnection
+        .selectCount("SELECT 1", [])
+        .then(data => {
+          if (data === 1) {
+            done();
+          } else {
+            done("count should be 1");
+          }
+        })
+        .catch(err => {
+          done(err);
         });
     });
 
-    describe("#select", () => {
-        const filepath = path.join(__dirname, "../../", "test", "northwind.db");
-        const mysqlConnection = new SqliteConnection(filepath, true);
-        it("should run if valid sql", (done) => {
-            mysqlConnection.select("SELECT 1", [])
-                .then((data) => {
-                    done();
-                })
-                .catch((err) => {
-                    done(err);
-                });
+    it("should has error if invalid sql", done => {
+      mysqlConnection
+        .selectCount("invalid sql", [])
+        .then(data => {
+          done("should have error if sql is invalid");
+        })
+        .catch(err => {
+          done();
         });
+    });
+  });
 
-        it("should has error if invalid sql", (done) => {
-            mysqlConnection.select("invalid sql", [])
-                .then((data) => {
-                    done("should have error if sql is invalid");
-                })
-                .catch((err) => {
-                    done();
-                });
+  describe("#selectEntities", async () => {
+    const filepath = path.join(__dirname, "../../", "test", "northwind.db");
+    const mysqlConnection = await new SqliteConnection(filepath, true);
+    it("should run if valid sql", done => {
+      mysqlConnection
+        .selectEntities<Customer>(Customer, "SELECT * FROM Customer", [])
+        .then(data => {
+          if (data.length > 0) {
+            done();
+          } else {
+            done("the count of Customer should greater than 0");
+          }
+        })
+        .catch(err => {
+          done(err);
         });
     });
 
-    describe("#selectCount", () => {
-        const filepath = path.join(__dirname, "../../", "test", "northwind.db");
-        const mysqlConnection = new SqliteConnection(filepath, true);
-        it("should run if valid sql", (done) => {
-            mysqlConnection.selectCount("SELECT 1", [])
-                .then((data) => {
-                    if (data === 1) {
-                        done();
-                    } else {
-                        done("count should be 1");
-                    }
-                })
-                .catch((err) => {
-                    done(err);
-                });
+    it("should has error if invalid sql", done => {
+      mysqlConnection
+        .selectEntities<Customer>(Customer, "invalid sql", [])
+        .then(data => {
+          done("should have error if sql is invalid");
+        })
+        .catch(err => {
+          done();
         });
+    });
+  });
 
-        it("should has error if invalid sql", (done) => {
-            mysqlConnection.selectCount("invalid sql", [])
-                .then((data) => {
-                    done("should have error if sql is invalid");
-                })
-                .catch((err) => {
-                    done();
-                });
+  describe("#transaction", () => {
+    const testHelper = new SqliteConnectionTestHelper();
+    it("insert", done => {
+      testHelper
+        .testTransactionInsert()
+        .then(() => {
+          done();
+        })
+        .catch(err => {
+          done(err);
         });
     });
 
-    describe("#selectEntities", () => {
-        const filepath = path.join(__dirname, "../../", "test", "northwind.db");
-        const mysqlConnection = new SqliteConnection(filepath, true);
-        it("should run if valid sql", (done) => {
-            mysqlConnection.selectEntities<Customer>(Customer, "SELECT * FROM Customer", [])
-                .then((data) => {
-                    if (data.length > 0) {
-                        done();
-                    } else {
-                        done("the count of Customer should greater than 0");
-                    }
-                })
-                .catch((err) => {
-                    done(err);
-                });
-        });
-
-        it("should has error if invalid sql", (done) => {
-            mysqlConnection.selectEntities<Customer>(Customer, "invalid sql", [])
-                .then((data) => {
-                    done("should have error if sql is invalid");
-                })
-                .catch((err) => {
-                    done();
-                });
+    it("insertThenRollback", done => {
+      testHelper
+        .testTransactionInsertThenRollback()
+        .then(() => {
+          done();
+        })
+        .catch(err => {
+          done(err);
         });
     });
-
-    describe("#transaction", () => {
-        const testHelper = new SqliteConnectionTestHelper();
-        it("insert", (done) => {
-            testHelper.testTransactionInsert()
-                .then(() => {
-                    done();
-                })
-                .catch((err) => {
-                    done(err);
-                });
-        });
-
-        it("insertThenRollback", (done) => {
-            testHelper.testTransactionInsertThenRollback()
-                .then(() => {
-                    done();
-                })
-                .catch((err) => {
-                    done(err);
-                });
-        });
-    });
+  });
 });
--- test/connection/sqliteConnectionTestHelper.ts
+++ test/connection/sqliteConnectionTestHelper.ts
@@ -3,58 +3,55 @@ import { SqliteConnection } from "../../src/connection/sqliteConnection";
 import { Student } from "../db/entity/student";
 
 export class SqliteConnectionTestHelper {
-
-    public async testTransactionInsert(): Promise<void> {
-        try {
-            const filepath = path.join(__dirname, "../../", "test", "northwind.db");
-            const sqliteConnection = new SqliteConnection(filepath, true);
-            const newStudent = new Student();
-            newStudent.name = new Date().toString();
-            newStudent.age = 30;
-            await sqliteConnection.beginTransaction();
-            const insertSqlTemplate = `INSERT INTO student values(?, ?)`;
-            await sqliteConnection.run(insertSqlTemplate, [newStudent.name, newStudent.age]);
-            await sqliteConnection.commit();
-            const selectMatchStudentTemplate = `SELECT * FROM student where name = ?`;
-            const matchStudent = await sqliteConnection.selectEntities<Student>(
-                Student, selectMatchStudentTemplate, [newStudent.name]);
-            await sqliteConnection.release();
-            return new Promise<void>((resolve, reject) => {
-                if (matchStudent.length === 1) {
-                    resolve();
-                } else {
-                    reject("could not find insert student");
-                }
-            });
-        } catch (e) {
-            return new Promise<void>((resolve, reject) => reject(e));
+  public async testTransactionInsert(): Promise<void> {
+    try {
+      const filepath = path.join(__dirname, "../../", "test", "northwind.db");
+      const sqliteConnection = await new SqliteConnection(filepath, true).init();
+      const newStudent = new Student();
+      newStudent.name = new Date().toString();
+      newStudent.age = 30;
+      await sqliteConnection.beginTransaction();
+      const insertSqlTemplate = `INSERT INTO student values(?, ?)`;
+      await sqliteConnection.run(insertSqlTemplate, [newStudent.name, newStudent.age]);
+      await sqliteConnection.commit();
+      const selectMatchStudentTemplate = `SELECT * FROM student where name = ?`;
+      const matchStudent = await sqliteConnection.selectEntities<Student>(Student, selectMatchStudentTemplate, [newStudent.name]);
+      await sqliteConnection.release();
+      return new Promise<void>((resolve, reject) => {
+        if (matchStudent.length === 1) {
+          resolve();
+        } else {
+          reject("could not find insert student");
         }
+      });
+    } catch (e) {
+      return new Promise<void>((resolve, reject) => reject(e));
     }
+  }
 
-    public async testTransactionInsertThenRollback(): Promise<void> {
-        try {
-            const filepath = path.join(__dirname, "../../", "test", "northwind.db");
-            const sqliteConnection = new SqliteConnection(filepath, true);
-            const newStudent = new Student();
-            newStudent.name = "rollback" + new Date().toString();
-            newStudent.age = 30;
-            await sqliteConnection.beginTransaction();
-            const insertSqlTemplate = `INSERT INTO student values(?, ?)`;
-            await sqliteConnection.run(insertSqlTemplate, [newStudent.name, newStudent.age]);
-            await sqliteConnection.rollback();
-            const selectMatchStudentTemplate = `SELECT * FROM student where name = ?`;
-            const matchStudent = await sqliteConnection.selectEntities<Student>(
-                Student, selectMatchStudentTemplate, [newStudent.name]);
-            await sqliteConnection.release();
-            return new Promise<void>((resolve, reject) => {
-                if (matchStudent.length === 0) {
-                    resolve();
-                } else {
-                    reject("should not find insert item since rollback");
-                }
-            });
-        } catch (e) {
-            return new Promise<void>((resolve, reject) => reject(e));
+  public async testTransactionInsertThenRollback(): Promise<void> {
+    try {
+      const filepath = path.join(__dirname, "../../", "test", "northwind.db");
+      const sqliteConnection = await new SqliteConnection(filepath, true).init();
+      const newStudent = new Student();
+      newStudent.name = "rollback" + new Date().toString();
+      newStudent.age = 30;
+      await sqliteConnection.beginTransaction();
+      const insertSqlTemplate = `INSERT INTO student values(?, ?)`;
+      await sqliteConnection.run(insertSqlTemplate, [newStudent.name, newStudent.age]);
+      await sqliteConnection.rollback();
+      const selectMatchStudentTemplate = `SELECT * FROM student where name = ?`;
+      const matchStudent = await sqliteConnection.selectEntities<Student>(Student, selectMatchStudentTemplate, [newStudent.name]);
+      await sqliteConnection.release();
+      return new Promise<void>((resolve, reject) => {
+        if (matchStudent.length === 0) {
+          resolve();
+        } else {
+          reject("should not find insert item since rollback");
         }
+      });
+    } catch (e) {
+      return new Promise<void>((resolve, reject) => reject(e));
     }
+  }
 }
