aa8abf7f953a0f37873967e83223d1f5e6e02c6b
wz2cool <wz2cool@live.cn>
2018-10-01 20:40:00 +0800

    add

--- src/helper/dtoObjectHelper.ts
+++ src/helper/dtoObjectHelper.ts
@@ -2,21 +2,22 @@ import { DtoObject } from "../model/dtoObject";
 import { ObjectUtils } from "ts-commons";
 
 export class DtoObjectHelper {
-    public static getDtoObjectName<T extends DtoObject>(o: T | { new(): T }): string {
-        if (ObjectUtils.isNullOrUndefined(o)) {
-            return "";
-        }
-
-        const testObj = typeof o === "function" ? new o() : o;
-        return (testObj.constructor as any).name;
+  public static getDtoObjectName<T extends DtoObject>(o: T | { new (): T }): string {
+    if (ObjectUtils.isNullOrUndefined(o)) {
+      return "";
     }
 
-    public static createObject<T extends DtoObject>(o: T | { new(): T }): T {
-        if (typeof o === "function") {
-            return new o();
-        } else {
-            const type = o.constructor as { new(): T };
-            return new type();
-        }
+    const testObj = this.createObject(o);
+    return (testObj.constructor as any).name;
+  }
+
+  public static createObject<T extends DtoObject>(o: T | { new (): T }): T {
+    if (typeof o === "function") {
+      const type = o as { new (): T };
+      return new type();
+    } else {
+      const type = o.constructor as { new (): T };
+      return new type();
     }
+  }
 }
