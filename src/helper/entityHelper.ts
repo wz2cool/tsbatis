fbf0470656f45f8e25997d0ff0ce468d82b73206
wz2cool <wz2cool@live.cn>
2018-10-01 22:54:59 +0800

    add

--- src/helper/entityHelper.ts
+++ src/helper/entityHelper.ts
@@ -2,34 +2,34 @@ import { Entity } from "../model";
 import { ObjectUtils } from "ts-commons";
 
 export class EntityHelper {
-  public static getPropertyName<T extends Entity>(fn: (o: T) => any): string {
-    if (ObjectUtils.isNullOrUndefined(fn)) {
-      return "";
-    }
+  // public static getPropertyName<T extends Entity>(fn: (o: T) => any): string {
+  //   if (ObjectUtils.isNullOrUndefined(fn)) {
+  //     return "";
+  //   }
 
-    const expression = fn.toString();
-    const returnIndex = expression.indexOf("return");
-    if (returnIndex > -1) {
-      const newExpression = expression.substring(returnIndex, expression.length - 1);
-      const regexp = new RegExp(`^return\\s+\\w+\.(\\w+)\\s*;\\s*$`);
-      const match = regexp.exec(newExpression);
+  //   const expression = fn.toString();
+  //   const returnIndex = expression.indexOf("return");
+  //   if (returnIndex > -1) {
+  //     const newExpression = expression.substring(returnIndex, expression.length - 1);
+  //     const regexp = new RegExp(`^return\\s+\\w+\.(\\w+)\\s*;\\s*$`);
+  //     const match = regexp.exec(newExpression);
 
-      if (match && match.length === 2) {
-        return match[1];
-      } else {
-        return "";
-      }
-    } else {
-      const regexp = new RegExp(`^\\s*\\(?\\w+\\)?\\s*=>\\s*\\w+\\.(\\w+)\\s*$`);
-      const match = regexp.exec(expression);
+  //     if (match && match.length === 2) {
+  //       return match[1];
+  //     } else {
+  //       return "";
+  //     }
+  //   } else {
+  //     const regexp = new RegExp(`^\\s*\\(?\\w+\\)?\\s*=>\\s*\\w+\\.(\\w+)\\s*$`);
+  //     const match = regexp.exec(expression);
 
-      if (match && match.length === 2) {
-        return match[1];
-      } else {
-        return "";
-      }
-    }
-  }
+  //     if (match && match.length === 2) {
+  //       return match[1];
+  //     } else {
+  //       return "";
+  //     }
+  //   }
+  // }
 
   public static getTargetConstrutor<T extends Entity>(o: T | { new (): T }): Function {
     if (ObjectUtils.isNullOrUndefined(o)) {
--- test/helper/entityHelperTest.ts
+++ test/helper/entityHelperTest.ts
@@ -8,23 +8,6 @@ class Student {
 }
 
 describe(".EntityHelper", () => {
-  describe("#getPropertyName", () => {
-    it("should return 'name'", () => {
-      const result = EntityHelper.getPropertyName<Student>(s => s.name);
-      expect("name").to.be.eq(result);
-    });
-
-    it("should return empty if input is null or empty", () => {
-      const result = EntityHelper.getPropertyName<Student>(null);
-      expect("").to.be.eq(result);
-    });
-
-    it("should return empty if expression is not valid", () => {
-      const result = EntityHelper.getPropertyName<Student>(s => 1);
-      expect("").to.be.eq(result);
-    });
-  });
-
   describe("#createObject", () => {
     it("should return object if o is type", () => {
       const result = EntityHelper.createObject<Student>(Student);
